70d5759504d82f584170e7b6076d871616e8b0c7
Himanshu Dongre <hdongr2@uic.edu>
2022-08-30 10:40:24 -0500

    index and sketch files added

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let x = 4;
+let y = 2;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+console.log("Hello, World!", x ** y);
+
+function draw() {
+    background(220);
+    line(200, 50, 200, 350);
+    line(50, 200, 350, 200);
+
+    line(200, 65, 215, 200);
+    line(200, 80, 230, 200);
+    line(200, 95, 245, 200);
+    line(200, 110, 260, 200);
+    line(200, 125, 275, 200);
+}
