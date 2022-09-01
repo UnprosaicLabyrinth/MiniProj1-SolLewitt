4f78d87f815fe749fe4a8e27f4ed0b5a83ed8fb7
Himanshu Dongre <hdongr2@uic.edu>
2022-08-31 21:54:32 -0500

    Edited sketch

--- sketch.js
+++ sketch.js
@@ -1,20 +1,32 @@
-let x = 4;
-let y = 2;
+const D = 800
+const s = 5;
 
-function setup() {
-    createCanvas(400, 400);
+const t = D / 8;
+const b = 7 * t;
+const c = D / 2;
+
+function setup() 
+{
+    createCanvas(D, D);
 }
 
-console.log("Hello, World!", x ** y);
-
-function draw() {
-    background(220);
-    line(200, 50, 200, 350);
-    line(50, 200, 350, 200);
-
-    line(200, 65, 215, 200);
-    line(200, 80, 230, 200);
-    line(200, 95, 245, 200);
-    line(200, 110, 260, 200);
-    line(200, 125, 275, 200);
+function draw() 
+{
+    background(0);
+    stroke(255, 255, 0);
+    let blue = true;
+    for (let i = 0; i <= c - t; i += s) 
+    {
+        if (blue) {
+            stroke(c - t - i, i, 255);
+            blue = false;
+        } else {
+            stroke(255, i, c - t - i);
+            blue = true;
+        }
+        line(c, t + i, c + i, c);
+        line(c - i, c, c, t + i);
+        line(c, b - i, c - i, c);
+        line(c + i, c, c, b - i);
+    }
 }
