f355703e308aeffb403878fddbc04882030e3337
Himanshu Dongre <hdongr2@uic.edu>
2022-09-03 01:28:05 -0500

    Edited sketch

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-const s = 20;
+const s = 5;
+
 function setup() 
 {
     createCanvas(windowWidth, windowHeight);
@@ -9,27 +10,14 @@ function draw()
     background(220);
     fill(255, 0);
     for (let i = 2 * windowWidth; i >= 2 * s; i -= (2 * s)) {
-        circle(2 * width, height / 2, i);
-        circle(-width, height / 2, i);
-        circle(width / 2, -height, i);
-        circle(width / 2, 2 * height, i);
-        circle(width / 2, height / 2, i);
+        circle(width / 2, 0, i); // N
+        // circle(width, 0, i); // NE
+        circle(width, height / 2, i);  // E
+        // circle(width, height, i);  // SE
+        circle(width / 2, height, i); // S 
+        // circle(0, height, i); // SW
+        circle(0, height / 2, i); // W
+        // circle(0, 0, i); // NW
+        circle(width / 2, height / 2, i); // C
     }
 }
-
-/*
-let yPos = 0;
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-  frameRate(30);
-}
-function draw() {
-  // draw() loops forever, until stopped
-  background(204);
-  yPos = yPos - 1;
-  if (yPos < 0) {
-    yPos = height;
-  }
-  line(0, yPos, width, yPos);
-}
-*/
