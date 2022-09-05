18dd04b07dc08dea6a2954d26498be003390b5dc
Himanshu Dongre <hdongr2@uic.edu>
2022-09-05 01:31:45 -0500

    Removed excess comments

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const s = 5;
+const s = 20;
 
 function setup() 
 {
@@ -10,14 +10,14 @@ function draw()
     background(220);
     fill(255, 0);
     for (let i = 2 * windowWidth; i >= 2 * s; i -= (2 * s)) {
-        circle(width / 2, 0, i); // N
-        // circle(width, 0, i); // NE
-        circle(width, height / 2, i);  // E
-        // circle(width, height, i);  // SE
-        circle(width / 2, height, i); // S 
-        // circle(0, height, i); // SW
-        circle(0, height / 2, i); // W
-        // circle(0, 0, i); // NW
-        circle(width / 2, height / 2, i); // C
+        circle(width / 2, 0, i);
+        // circle(width, 0, i);
+        circle(width, height / 2, i);
+        // circle(width, height, i);
+        circle(width / 2, height, i);
+        // circle(0, height, i);
+        circle(0, height / 2, i);
+        // circle(0, 0, i);
+        circle(width / 2, height / 2, i);
     }
 }
