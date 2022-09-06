934954a8ec20aca61a1479c5e8c047e620183941
Himanshu Dongre <hdongr2@uic.edu>
2022-09-06 08:45:54 -0500

    Removed comments from sketch

--- sketch.js
+++ sketch.js
@@ -11,13 +11,9 @@ function draw()
     fill(255, 0);
     for (let i = 2 * width; i >= 2 * s; i -= (2 * s)) {
         circle(width / 2, 0, i);
-        // circle(width, 0, i);
         circle(width, height / 2, i);
-        // circle(width, height, i);
         circle(width / 2, height, i);
-        // circle(0, height, i);
         circle(0, height / 2, i);
-        // circle(0, 0, i);
         circle(width / 2, height / 2, i);
     }
 }
